8181970f45aee21c0556d00c4dc521679da81bc8
SnowFlowerr <bhudeokrit@gmail.com>
2024-09-22 10:25:20 +0530

    first commit

--- src/component/Wordie.jsx
+++ src/component/Wordie.jsx
@@ -4,21 +4,26 @@ import Box from './Box'
 import axios from 'axios'
 
 export default function Wordie() {
-    const [word, setWord] = useState("april")
+    const [word, setWord] = useState("")
     const [value, setValue] = useState(["", "", "", "", ""])
     const [boxes, setBoxes] = useState([])
     const [win, setWin] = useState(false)
     const [lose, setLose] = useState(false)
     const [isword, setisword] = useState(false)
+    const [currentind, setCurrentind] = useState(0)
     const inpRef = useRef([React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef()])
 
     useEffect(() => {
         getWords()
+        
     }, [])
     async function getWords() {
         try {
             const w = await axios.get('https://random-word-api.vercel.app/api?words=1&length=5')
-            // setWord(w.data[0])
+            setWord(w.data[0])
+            if(inpRef.current[0].current){
+                await inpRef.current[0].current.focus()
+            }
         }
         catch (err) {
             console.log("err")
@@ -44,12 +49,13 @@ export default function Wordie() {
             arr[ind] = e.target.value
             if (ind < 4) {
                 inpRef.current[ind + 1].current.focus()
+                inpRef.current[ind + 1].current.setSelectionRange(1, 1)
             }
         }
         else {
             arr[ind] = ""
             if (ind > 0) {
-                inpRef.current[ind - 1].current.focus()
+                // inpRef.current[ind - 1].current.focus()
             }
         }
         setValue(() => [...arr])
@@ -59,13 +65,14 @@ export default function Wordie() {
             isWords()
         }
     }, [value])
-    function handleSubmit() {
-        if (boxes.length <= 4) {
-            setBoxes([...boxes, value])
-            setValue(["", "", "", "", ""])
-            setisword(false)
+    async function handleSubmit() {
+        if (boxes.length <= 5) {
+            setBoxes(()=>[...boxes, value])
+            setValue(()=>["", "", "", "", ""])
+            setisword(()=>false)
+            await inpRef.current[0].current.focus()
         }
-        if(boxes.length >= 4){
+        if(boxes.length >= 5){
             setLose(true)
             setisword(true)
         }
@@ -82,26 +89,46 @@ export default function Wordie() {
         setLose(false)
         setisword(false)
     }
+    function handleFocus(ind) {
+        setCurrentind(ind)
+    }
+    function key(e) {
+        const key=e.key
+        // console.log(key)
+        if(key===" " && currentind < 4){
+            // inpRef.current[currentind + 1].current.focus()
+            console.log(key)
+        }
+        else if((key.toLowerCase()==="delete" || key.toLowerCase()==="backspace") && currentind > 0 && value[currentind]===""){
+            inpRef.current[currentind - 1].current.focus()
+            // console.log(currentind)
+        }
+    }
     return (
         <div className={styles.mainBox}>
             <div className={styles.container}>
+                
+                <>
                 <div className={styles.smallCont}>
                     {boxes.map((element, index) =>
                         <div className={styles.column} key={index}>
                             {element.map((ele, ind) =>
-                                <Box key={index} index={ind} element={ele} word={word} />
+                                <Box key={ind} index={ind} element={ele} word={word} />
                             )}
                         </div>
                     )}
                 </div>
                 <div className={styles.column}>
-                    {boxes.length <= 4 && !win &&
+                    {boxes.length <= 5 && !win &&
                         word.split("").map((element, index) =>
-                            <input type="text" key={index} className={styles.letters} maxLength={1} value={value[index]} onChange={(e) => handleChange(e, index)} ref={inpRef.current[index]}/>
+                            <input type="text" key={index} className={styles.letters} maxLength={1} value={value[index]} onChange={(e) => handleChange(e, index)} ref={inpRef.current[index]} onFocus={()=>handleFocus(index)} onKeyDown={key}/>
                         )}
                 </div>
+                </>
+                {(win || lose) && <div className={styles.message}>{win && "YOU WON"}{lose && "YOU LOSE"}</div>}
+                
             </div>
-            <button className={styles.submit} onClick={win || lose ? handleRestart : isword ? handleSubmit : null} style={isword ? {} : { background: "gray" }}>{win || lose ? "RESTART" : "SUBMIT"}{lose && word}</button>
+            <button className={styles.submit} onClick={win || lose ? handleRestart : isword ? handleSubmit : null} style={isword ? {} : { background: "gray" }}>{win || lose ? "RESTART" : "SUBMIT"}</button>
             
         </div>
     )
